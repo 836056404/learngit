ed38dc77a62119473d9b475ed7560c48f5e01c8c
liweitong <836056404@qq.com>
2017-07-17 09:10:51 +0800

    banben1

---
+++ git.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/17
+ * Time: 8:46
+ */
+ if(1){
+     echo 'git is a version control system112';
+ }else{
+    echo 'git is free software112';
+ }
